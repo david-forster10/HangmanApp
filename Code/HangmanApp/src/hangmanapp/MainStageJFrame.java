c408294e046f7c4351ef5c6271c47f90baca7a4a
manlikeaz <a.parcsami@hotmail.co.uk>
2017-03-22 10:59:39 +0000

    Main Stage

--- Code/HangmanApp/src/hangmanapp/MainStageJFrame.java
+++ Code/HangmanApp/src/hangmanapp/MainStageJFrame.java
@@ -5,8 +5,9 @@ public class MainStageJFrame extends javax.swing.JFrame {
     /**
      * Creates new form MainStageJFrame
      */
-    public MainStageJFrame() {
-        initComponents();
+    public MainStageJFrame() 
+    {
+        initComponents();      
     }
 
     /**
@@ -20,27 +21,233 @@ public class MainStageJFrame extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         lblWord = new javax.swing.JLabel();
+        btn_q = new javax.swing.JButton();
+        btn_w = new javax.swing.JButton();
+        btn_e = new javax.swing.JButton();
+        btn_r = new javax.swing.JButton();
+        btn_t = new javax.swing.JButton();
+        btn_y = new javax.swing.JButton();
+        btn_u = new javax.swing.JButton();
+        btn_i = new javax.swing.JButton();
+        btn_o = new javax.swing.JButton();
+        btn_p = new javax.swing.JButton();
+        btn_a = new javax.swing.JButton();
+        btn_s = new javax.swing.JButton();
+        btn_d = new javax.swing.JButton();
+        btn_f = new javax.swing.JButton();
+        btn_g = new javax.swing.JButton();
+        btn_h = new javax.swing.JButton();
+        btn_j = new javax.swing.JButton();
+        btn_k = new javax.swing.JButton();
+        btn_l = new javax.swing.JButton();
+        btn_z = new javax.swing.JButton();
+        btn_x = new javax.swing.JButton();
+        btn_c = new javax.swing.JButton();
+        btn_v = new javax.swing.JButton();
+        btn_b = new javax.swing.JButton();
+        btn_m = new javax.swing.JButton();
+        btn_n = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        lbl_guessesleftgraphic = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        lbl_timergraphic = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(961, 665));
 
-        lblWord.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
-        lblWord.setText("_ _ _ _");
+        lblWord.setFont(new java.awt.Font("Tahoma", 0, 75)); // NOI18N
+        lblWord.setText("_ _ _ _ _ _ _ _ _");
+
+        btn_q.setText("q");
+
+        btn_w.setText("w");
+
+        btn_e.setText("e");
+
+        btn_r.setText("r");
+
+        btn_t.setText("t");
+
+        btn_y.setText("y");
+
+        btn_u.setText("u");
+
+        btn_i.setText("i");
+
+        btn_o.setText("o");
+
+        btn_p.setText("p");
+
+        btn_a.setText("a");
+
+        btn_s.setText("s");
+
+        btn_d.setText("d");
+
+        btn_f.setText("f");
+
+        btn_g.setText("g");
+
+        btn_h.setText("h");
+
+        btn_j.setText("j");
+
+        btn_k.setText("k");
+
+        btn_l.setText("l");
+
+        btn_z.setText("z");
+
+        btn_x.setText("x");
+
+        btn_c.setText("c");
+
+        btn_v.setText("v");
+
+        btn_b.setText("b");
+
+        btn_m.setText("m");
+
+        btn_n.setText("n");
+
+        jTextField1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+
+        lbl_guessesleftgraphic.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
+        lbl_guessesleftgraphic.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lbl_guessesleftgraphic.setIcon(new javax.swing.ImageIcon(getClass().getResource("/hangmanapp/images/GuessLeft.png"))); // NOI18N
+        lbl_guessesleftgraphic.setText("9");
+        lbl_guessesleftgraphic.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
+        lbl_guessesleftgraphic.setIconTextGap(-230);
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jButton1.setText("Guess the Word");
+
+        lbl_timergraphic.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
+        lbl_timergraphic.setIcon(new javax.swing.ImageIcon(getClass().getResource("/hangmanapp/images/TimeTaken.png"))); // NOI18N
+        lbl_timergraphic.setText("0:00");
+        lbl_timergraphic.setIconTextGap(-115);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(32, 32, 32)
-                .addComponent(lblWord)
-                .addContainerGap(515, Short.MAX_VALUE))
+                .addComponent(lbl_guessesleftgraphic)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(lbl_timergraphic))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 344, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(btn_a)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_s)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_d)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_f)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_g)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_h)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_j)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_k))
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGap(24, 24, 24)
+                                        .addComponent(btn_z)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_x)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_c)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_v)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_b)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_n)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(btn_m)))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btn_l))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(115, 115, 115)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(btn_q)
+                        .addGap(2, 2, 2)
+                        .addComponent(btn_w)
+                        .addGap(2, 2, 2)
+                        .addComponent(btn_e)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btn_r)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btn_t)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btn_y)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btn_u)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btn_i)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btn_o)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btn_p))
+                    .addComponent(lblWord))
+                .addContainerGap(410, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(86, 86, 86)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbl_guessesleftgraphic)
+                    .addComponent(lbl_timergraphic))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 90, Short.MAX_VALUE)
                 .addComponent(lblWord)
-                .addContainerGap(250, Short.MAX_VALUE))
+                .addGap(112, 112, 112)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btn_q)
+                    .addComponent(btn_w)
+                    .addComponent(btn_e)
+                    .addComponent(btn_r)
+                    .addComponent(btn_t)
+                    .addComponent(btn_y)
+                    .addComponent(btn_i)
+                    .addComponent(btn_o)
+                    .addComponent(btn_u)
+                    .addComponent(btn_p))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btn_a)
+                    .addComponent(btn_s)
+                    .addComponent(btn_d)
+                    .addComponent(btn_f)
+                    .addComponent(btn_g)
+                    .addComponent(btn_h)
+                    .addComponent(btn_j)
+                    .addComponent(btn_k)
+                    .addComponent(btn_l))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btn_z)
+                    .addComponent(btn_n)
+                    .addComponent(btn_x)
+                    .addComponent(btn_c)
+                    .addComponent(btn_v)
+                    .addComponent(btn_b)
+                    .addComponent(btn_m))
+                .addGap(18, 18, 18)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(36, 36, 36))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -93,7 +300,37 @@ public class MainStageJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_a;
+    private javax.swing.JButton btn_b;
+    private javax.swing.JButton btn_c;
+    private javax.swing.JButton btn_d;
+    private javax.swing.JButton btn_e;
+    private javax.swing.JButton btn_f;
+    private javax.swing.JButton btn_g;
+    private javax.swing.JButton btn_h;
+    private javax.swing.JButton btn_i;
+    private javax.swing.JButton btn_j;
+    private javax.swing.JButton btn_k;
+    private javax.swing.JButton btn_l;
+    private javax.swing.JButton btn_m;
+    private javax.swing.JButton btn_n;
+    private javax.swing.JButton btn_o;
+    private javax.swing.JButton btn_p;
+    private javax.swing.JButton btn_q;
+    private javax.swing.JButton btn_r;
+    private javax.swing.JButton btn_s;
+    private javax.swing.JButton btn_t;
+    private javax.swing.JButton btn_u;
+    private javax.swing.JButton btn_v;
+    private javax.swing.JButton btn_w;
+    private javax.swing.JButton btn_x;
+    private javax.swing.JButton btn_y;
+    private javax.swing.JButton btn_z;
+    private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JLabel lblWord;
+    private javax.swing.JLabel lbl_guessesleftgraphic;
+    private javax.swing.JLabel lbl_timergraphic;
     // End of variables declaration//GEN-END:variables
 }
