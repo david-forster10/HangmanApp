49837d883883cd1308d024d6b912475f5df43a2a
Luke Roberts <11lroberts@madeleyacademy.com>
2017-03-22 08:41:28 +0000

    Revert "Whoops"
    This reverts commit 7f14ef8bd43928da0707f47fe07d9f17d82a4ac6.

---
+++ Code/HangmanApp/src/hangmanapp/HangmanApp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangmanapp;
+
+/**
+ *
+ * @author Vanilla
+ */
+public class HangmanApp 
+{
+    public static void main(String[] args) 
+    {
+        StartJFrame form = new StartJFrame();
+        form.setVisible(true);
+    }
+}
